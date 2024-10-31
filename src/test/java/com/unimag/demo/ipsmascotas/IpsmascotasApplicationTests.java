e34c4bce403885b5167555aaa8e7bf680aa83c5a
orionwilx <wilman3d@hotmail.com>
2024-10-31 16:53:48 -0500

    frist commit

---
+++ src/test/java/com/unimag/demo/ipsmascotas/IpsmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimag.demo.ipsmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
